fd807eca928cd9286bd9324f7da512ad9ab29dff
wangyou <wangyou2550@163.com>
2020-06-17 11:45:03 +0800

    本地工程到git11

--- src/com/wangyou/git/Main.java
+++ src/com/wangyou/git/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("123");
     }
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
 }
